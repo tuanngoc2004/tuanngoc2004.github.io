c48f91e9a6ebe552e88df9d7354858f5cb8f2bee
Ngocchim-12345 <anninhthaiduong@gmail.com>
2023-05-05 18:53:50 +0700

    day14

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Lưu ý nếu code ở file js lỗi => toàn bộ lỗi
+        // alert('asđ');
+        // Khai báo biến
+        // Có 3 kiểu : var, let, const,
+var number1 = 1; //thông dụng
+let number2 =  2;
+const numbers = 3;
+// let a = 1;
+
+// function handleCount () {
+//     console.log(a);
+//     let a = 1;
+// }
+
+// handleCount();
+
+// hướng đôi tương class
+var string = "hello world";
+var number = 1000;
+// console.log(number);
+var bool = true;
+var array = [1, 2, 3, 4, 5, 6];
+var array = [[1, 2, 3, 4],[4,5,6,7]];
+var number7 = array[0][1];
+var arrayString = ['red','green','blue'];
+var greenColor = arrayString[1];
+console.log(greenColor);
+// Expected output: Array [30, 40, 50]// console.log(number);
+var string = ' 1000.5';
+var nuber5 = parseFloat(string);
+console.log(nuber5);
+
+var d = 2;
+var b = 3;
+var sum = d+b;
+var divide = d/b;
+var string = 'đây là số:';
+console.log('Tổng là: ' + sum);
+console.log(string + d);
+
+
